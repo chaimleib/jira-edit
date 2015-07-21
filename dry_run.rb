0d4871a7c8f63a7694ace935828006f2e2d80017
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2015-07-21 11:43:14 -0700

    Better status output

--- dry_run.rb
+++ dry_run.rb
@@ -2,7 +2,6 @@
 $:.unshift File.join(__FILE__, '..', 'lib')
   
 require 'potato_core/jira_adapter'
-require 'uri'
 require 'cgi'
 require 'pry'
 
@@ -18,8 +17,8 @@ order = 'Rank ASC, priority ASC'
 jql = "#{conditions.join ' AND '} ORDER BY #{order}"
 puts jql
 path = "/rest/api/2/search"
-puts path
-joined_path = "#{path}?jql=#{jql}"
+joined_path = "#{path}?jql=#{CGI.escape jql}"
+puts joined_path
 
 j = JiraAdapter.new
 start_at = 0
@@ -28,6 +27,7 @@ fields = ['key', 'labels']
 
 all_results = []
 loop do
+  puts "Fetching #{max_results} starting at #{start_at}..."
   r = j.get_issues conditions, order, {start_at: start_at, max_results: max_results, fields: fields}
   break if r.length == 0
   all_results.push *r
@@ -36,6 +36,6 @@ end
 
 all_results.each_with_index{|i, index|
   labels = i.labels + ['metal_bunnies']
-  # i.save({fields: {labels: labels}})
+  # failed << i unless i.save({fields: {labels: labels}})
   puts "#{index+1}/#{all_results.length}: #{i.key} #{i.labels}"
 }
--- edit.rb
+++ edit.rb
@@ -2,7 +2,6 @@
 $:.unshift File.join(__FILE__, '..', 'lib')
   
 require 'potato_core/jira_adapter'
-require 'uri'
 require 'cgi'
 require 'pry'
 
@@ -18,8 +17,8 @@ order = 'Rank ASC, priority ASC'
 jql = "#{conditions.join ' AND '} ORDER BY #{order}"
 puts jql
 path = "/rest/api/2/search"
-puts path
-joined_path = "#{path}?jql=#{jql}"
+joined_path = "#{path}?jql=#{CGI.escape jql}"
+puts joined_path
 
 j = JiraAdapter.new
 start_at = 0
@@ -28,14 +27,20 @@ fields = ['key', 'labels']
 
 all_results = []
 loop do
+  puts "Fetching #{max_results} starting at #{start_at}..."
   r = j.get_issues conditions, order, {start_at: start_at, max_results: max_results, fields: fields}
   break if r.length == 0
   all_results.push *r
   start_at += max_results
 end
 
+failed = []
 all_results.each_with_index{|i, index|
   labels = i.labels + ['metal_bunnies']
-  i.save({fields: {labels: labels}})
+  failed << i unless i.save({fields: {labels: labels}})
   puts "#{index+1}/#{all_results.length}: #{i.key} #{i.labels}"
 }
+
+unless failed.empty?
+  puts "#{failed.length} issues could not be edited"
+end
